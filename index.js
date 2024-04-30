0a15910984c3fc5a79f7ba39d0ba9880471daaed
Varun Thakur <varunkumar84150@gmail.com>
2024-05-01 01:32:59 +0530

    Implement GET /api/users/:id endpoint to retrieve a specific user by id

--- index.js
+++ index.js
@@ -61,6 +61,12 @@ app.get('/users', (req, res) => {
 app.get('/api/users', (req, res) => {
     return res.json(users);
 })
+
+app.get('/api/users/:id', (req, res) => {
+    const id = Number(req.params.id);
+    const user = users.find((user) => user.id === id);
+    return res.json(user);
+})
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
